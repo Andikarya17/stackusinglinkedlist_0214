a4c4f37678c3c621c12b01d70eeca91ff9f9873c
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-11 10:12:59 +0700

    membuat class node

--- stackusinglinkedlist_0214.cpp
+++ stackusinglinkedlist_0214.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+//node class representing a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
