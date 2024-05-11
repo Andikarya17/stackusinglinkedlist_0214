613cddd70b5ea8dd96cf074c089598999edc467f
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-11 10:53:08 +0700

    Dapatkan nilai elemen teratas

--- stackusinglinkedlist_0214.cpp
+++ stackusinglinkedlist_0214.cpp
@@ -84,5 +84,17 @@ int main() {
             if (!stack.isEmpty()) {
                 stack.pop();
             }
+            else {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        case 3:
+            if (!stack.isEmpty()) {
+                stack.peek();
+            }
+            else {
+                cout << "Stack is empty. No top value." << endl;
+            }
+            break;
 
 }
