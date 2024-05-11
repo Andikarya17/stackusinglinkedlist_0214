8c821de7987f9387b31ac90865048de27e5dff67
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-11 10:51:58 +0700

    pop elemen teratas dari tumpukan

--- stackusinglinkedlist_0214.cpp
+++ stackusinglinkedlist_0214.cpp
@@ -80,5 +80,9 @@ int main() {
             cin >> value;
             stack.push(value);
             break;
+        case 2:
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
 
 }
